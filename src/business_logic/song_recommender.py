9890cc043c556adca19552666c932a8bce8a3422
caioissa <caioissa96@gmail.com>
2020-07-30 13:04:55 -0300

    added test script and updated requirements.txt

--- src/business_logic/song_recommender.py
+++ src/business_logic/song_recommender.py
@@ -2,24 +2,22 @@ import sys
 sys.path.append('./src')
 from api_interface.openweather_interface import OpenweatherInterface
 from api_interface.spotify_interface import SpotifyInterface
-import ipdb
 
 class SongRecommender():
     def __init__(self):
         self.oi = OpenweatherInterface()
         self.si = SpotifyInterface()
 
-    def recommend_playlist_by_cityname(self, cityname, length):
+    def recommend_playlist_by_cityname(self, cityname, length=20):
         temp = self.oi.get_temp_by_cityname(cityname)
         return self.get_playlist_by_temperature(temp, length)
 
-    def recommend_playlist_by_latlon(self, lat, lon, lenght):
+    def recommend_playlist_by_latlon(self, lat, lon, length=20):
         temp = self.oi.get_temp_by_latlon(lat, lon)
         return self.get_playlist_by_temperature(temp, length)
 
-    def get_playlist_by_temperature(self, temp, length):
+    def get_playlist_by_temperature(self, temp, length=20):
         genre = self.get_genre_by_temperature(temp)
-        ipdb.set_trace()
         return {'genre': genre, 'playlist': self.si.get_playlist_by_genre(genre, length)}
 
     def get_genre_by_temperature(self, temp):
