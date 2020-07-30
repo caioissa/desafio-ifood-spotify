08876c68dd2df9a11e8089e19a5d4075c6537dc8
caioissa <caioissa96@gmail.com>
2020-07-30 12:10:49 -0300

    business logic and api docs

---
+++ src/business_logic/song_recommender.py
@@ -0,0 +1,33 @@
+import sys
+sys.path.append('./src')
+from api_interface.openweather_interface import OpenweatherInterface
+from api_interface.spotify_interface import SpotifyInterface
+import ipdb
+
+class SongRecommender():
+    def __init__(self):
+        self.oi = OpenweatherInterface()
+        self.si = SpotifyInterface()
+
+    def recommend_playlist_by_cityname(self, cityname, length):
+        temp = self.oi.get_temp_by_cityname(cityname)
+        return self.get_playlist_by_temperature(temp, length)
+
+    def recommend_playlist_by_latlon(self, lat, lon, lenght):
+        temp = self.oi.get_temp_by_latlon(lat, lon)
+        return self.get_playlist_by_temperature(temp, length)
+
+    def get_playlist_by_temperature(self, temp, length):
+        genre = self.get_genre_by_temperature(temp)
+        ipdb.set_trace()
+        return {'genre': genre, 'playlist': self.si.get_playlist_by_genre(genre, length)}
+
+    def get_genre_by_temperature(self, temp):
+        if temp > 30:
+            return 'party'
+        elif temp >= 15:
+            return 'pop'
+        elif temp >= 10:
+            return 'rock'
+        else:
+            return 'classical'
