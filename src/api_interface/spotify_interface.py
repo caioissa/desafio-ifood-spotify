96ec7185cb48e2f2f0e6d528494e21b0bd76cdc3
caioissa <caioissa96@gmail.com>
2020-08-18 16:37:46 -0300

    added test cases

--- src/api_interface/spotify_interface.py
+++ src/api_interface/spotify_interface.py
@@ -3,7 +3,11 @@ from spotipy.oauth2 import SpotifyClientCredentials
 
 class SpotifyInterface():
     def __init__(self):
-        self.sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials())
+        self.sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(
+            client_id='20fd0b23aa5a44a3a369e483e9d216e5',
+            client_secret='cf98548e2ab343d8b2afe04808637f04'
+            )
+        )
 
     def get_playlist_by_genre(self, genre, length=20):
         lista = self.sp.recommendations(seed_genres=[genre], limit=length)['tracks']
--- src/tests/test_app.py
+++ src/tests/test_app.py
@@ -1,8 +1,22 @@
+import sys
+
 import unittest
 
+sys.path.append('./src')
+from app import app
+
 class TestApp(unittest.TestCase):
+    def setUp(self):
+        self.app = app.test_client()
+        self.app.testing = True
+
     def test_run(self):
-        self.assertEqual(200, 200)
+        response = self.app.get('/playlist?cityname=Campinas')
+        self.assertEqual(response.status_code, 200)
+
+    def test_playlist_length(self):
+        response = self.app.get('/playlist?lat=20&lon=20')
+        self.assertEqual(len(response.json['playlist']), 20)
 
 if __name__ == '__main__':
     unittest.main()
