682cb85a00418e6218049f8741c147da18e7cb34
caioissa <caioissa96@gmail.com>
2020-08-28 10:49:31 -0300

    Added test cases

--- src/tests/test_app.py
+++ src/tests/test_app.py
@@ -18,5 +18,25 @@ class TestApp(unittest.TestCase):
         response = self.app.get('/playlist?lat=20&lon=20')
         self.assertEqual(len(response.json['playlist']), 20)
 
+    def test_unknown_cityname(self):
+        response = self.app.get('/playlist?cityname=cdisaocjsaiocdjaio')
+        self.assertEqual(response.status_code, 500)
+
+    def test_invalid_lat(self):
+        response = self.app.get('/playlist?lat=250&lon=20')
+        self.assertEqual(response.status_code, 500)
+
+    def test_invalid_lon(self):
+        response = self.app.get('/playlist?lat=20&lon=250')
+        self.assertEqual(response.status_code, 500)
+
+    def test_playlist_length_query_100(self):
+        response = self.app.get('/playlist?cityname=Campinas&length=100')
+        self.assertEqual(len(response.json['playlist']), 100)
+
+    def test_playlist_length_query_1(self):
+        response = self.app.get('/playlist?cityname=Campinas&length=1')
+        self.assertEqual(len(response.json['playlist']), 1)
+
 if __name__ == '__main__':
     unittest.main()
